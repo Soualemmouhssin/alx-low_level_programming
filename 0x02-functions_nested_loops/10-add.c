315629da370a314e83ebbfb66cbf3e80abfd5aa6
Soualemmouhssin <elseniormouhssin@gmail.com>
2023-03-31 07:30:08 -0700

    test

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _add - check if charachter is lowercase
+ * @a: The charachter to check
+ * @b: Second integer
+ *
+ * Return: 1 if islower case 0 if not.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int add(int a, int b)
+{
+	return (a+b) ;
+}
