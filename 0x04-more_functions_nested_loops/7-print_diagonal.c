b127ca86ab72b3fc029b2b9fad1c588d768ecca7
Soualemmouhssin <elseniormouhssin@gmail.com>
2023-04-01 07:25:17 -0700

    test

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -13,12 +13,12 @@ void print_diagonal(int n)
 	int i, j;
 
 	if (n <= 0)
-	       	_putchar('\n');
+		_putchar('\n');
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < i; j++)
 		{
-		       	_putchar(' ');
+			_putchar(' ');
 		}
 		_putchar('\\');
 		_putchar('\n');
